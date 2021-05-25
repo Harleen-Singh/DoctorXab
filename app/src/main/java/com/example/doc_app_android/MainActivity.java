2e1691e4d2c280683f2ad29ab39239d63d0ca60f
RishabhRajDhiman <dhimanrishabhraj@gmail.com>
2021-05-25 16:16:23 +0530

    Added Login Screen

---
+++ app/src/main/java/com/example/doc_app_android/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.doc_app_android;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(R.layout.sign_in_as);
+    }
+}
