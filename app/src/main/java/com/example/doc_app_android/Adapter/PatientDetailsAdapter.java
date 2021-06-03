9236f83ba2211dad12863120f5f3e7b287001883
RishabhRajDhiman <dhimanrishabhraj@gmail.com>
2021-06-03 22:44:30 +0530

    buttons effects

--- app/src/main/java/com/example/doc_app_android/Adapter/PatientDetailsAdapter.java
+++ app/src/main/java/com/example/doc_app_android/Adapter/PatientDetailsAdapter.java
@@ -30,7 +30,7 @@ public class PatientDetailsAdapter extends RecyclerView.Adapter<PatientDetailsAd
     public PatientDetailsHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.home_single_row, parent, false);
+        View view = inflater.inflate(R.layout.home_single_patient_row, parent, false);
         return new PatientDetailsHolder(view);
 
     }
