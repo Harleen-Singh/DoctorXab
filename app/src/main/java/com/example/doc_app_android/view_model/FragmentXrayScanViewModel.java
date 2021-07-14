fd2e64ef6527df86feaee54d5be6d7a2a94c888c
Priyanshu-2001 <priyanshuhupta123123@gmail.com>
2021-07-14 16:17:45 +0530

    enjoi frands

---
+++ app/src/main/java/com/example/doc_app_android/view_model/FragmentXrayScanViewModel.java
@@ -0,0 +1,25 @@
+package com.example.doc_app_android.view_model;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.doc_app_android.data_model.Xray_data;
+import com.example.doc_app_android.services.xray_service;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class FragmentXrayScanViewModel extends AndroidViewModel {
+    xray_service service = new xray_service();
+    public FragmentXrayScanViewModel(@NonNull @NotNull Application application) {
+        super(application);
+    }
+
+    public LiveData<ArrayList<Xray_data>> get_Xray_data(){
+        return service.getX_ray_data();
+    }
+}
